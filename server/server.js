25fc338f367592a82f8cfd4d96bb31226af169ad
Reine0017 <m.fortitudo.fr@gmail.com>
2020-08-13 14:57:04 +0700

    setup done

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const config = require("./config/config").get(process.env.NODE_ENV);
+const app = express();
+
+mongoose.connect(config.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+//MIDDLEWARE
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3005;
+app.listen(port, () => {
+  console.log("SERVER RUNNING");
+});
